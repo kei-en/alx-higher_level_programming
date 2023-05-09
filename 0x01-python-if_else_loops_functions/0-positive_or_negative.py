9c030a244872c50a99210f5a9c3d58d7c2700877
kei-en <njuguna0karanja@gmail.com>
2023-05-09 08:17:21 -0700

    The last digit

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-# get the last digit
-last_digit = abs(number) % 10
-# Check if the last digit of number is greater than 5
-if last_digit > 5:
-	print(f"Last digit of {number} is {last_digit} and is greater than 5")
-# Check if the last digit is zero
-elif last_digit == 0:
-	print(f"Last digit of {number} is {last_digit} and is 0")
-# Check if last digit is less than 6 and not 0
-elif last_digit < 6 and last_digit != 0:
-	print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+if number > 0:
+	print(f"{number} is positive")
+elif number < 0:
+	print(f"{number} is negative")
+else:
+	print(f"{number} is zero")
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -1,3 +1,4 @@
 # Python - if/else, loops, functions
 ## Contains
-- ***0-positive_or_negative.py*** - prints whether the last digit stored in the variable *number* is positive or negative
+- ***0-positive_or_negative.py*** - prints whether the number store in the variable *number* is positive or negative
+- ***1-last_digit.py*** - prints the last digit stored in the variable *number* is positive or negative
