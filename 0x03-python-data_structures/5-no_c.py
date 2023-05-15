8375bc226ef7c76b622ac9ca16b75fe6cf337217
kei-en <njuguna0karanja@gmail.com>
2023-05-15 03:36:54 -0700

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,9 +2,6 @@
 
 def no_c(my_string):
     """Removes all characters c and C"""
-    replacements = [("c", ""), ("C", "")]
-    for char, replacement in replacements:
-        if char in my_string:
-            my_string = my_string.replace(char, replacement)
+    my_new_string = [i for i in my_string if i != 'c' and i != 'C']
 
-    return my_string
+    return ("".join(my_new_string))
