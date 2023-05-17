aa9096849397ce4a00081a776460c985a94871a6
kei-en <njuguna0karanja@gmail.com>
2023-05-17 11:33:32 -0700

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    """Replaces all occurrences of an element with another"""
+    new_list = my_list.copy()
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return (new_list)
--- 0x04-python-more_data_structures/README.md
+++ 0x04-python-more_data_structures/README.md
@@ -1,4 +1,5 @@
 # Python - More Data Structures: Set, Dictionary
 ## Contains:
 - ***0-square_matrix_simple.py*** - computes the square of all integers of a matrix
+- ***1-search_replace.py*** - replaces all occurrences of an element by another in a new list
 
