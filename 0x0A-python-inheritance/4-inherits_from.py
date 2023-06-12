9bb28e5ef4db30414c30d9c960ea73d78167caf2
Kei En <njuguna0karanja@gmail.com>
2023-06-13 02:05:53 +0300

    Pycodestyle requirement error

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,7 +8,7 @@ def inherits_from(obj, a_class):
         obj (any): The object to check
         a_class (type): The class to mathcht the type of obj to
     Return:
-        True - if obj is an inherited instance of a_class 
+        True - if obj is an inherited instance of a_class
         False - otherwhise
     """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
