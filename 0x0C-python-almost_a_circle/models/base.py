6c66003a67bb7b22c61ac63f725c0a39cec4249a
Kei En <njuguna0karanja@gmail.com>
2023-06-19 12:21:43 +0300

    Dictionary to instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -62,3 +62,18 @@ class Base:
         if json_string is None or json_string == "[]":
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return an instance with all attributes already set
+
+        Args:
+            **dictionary (dict): key/value pairs of attributes to initialize
+        """
+        if dictionary and dictionary != []:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
