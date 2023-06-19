c5b1abae2ded8177c7feb6b374ea7e0f4e6ae5c7
Kei En <njuguna0karanja@gmail.com>
2023-06-19 11:41:00 +0300

    And now, the Square!

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""Define class Square than inherits from Rectangle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represent a aquare"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square
+        Args:
+            size (int): The size of the new square
+            x (int): The x-coordinate of the new square
+            y (int): The y-coordinate of the new square
+            id (int): The id of the new square
+        """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Set/get the size of the square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update the square
+
+        Args:
+            *args (ints): New attribute values
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for i, j in kwargs.items():
+                if i == "id":
+                    if j is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = j
+                elif i == "size":
+                    self.size = j
+                elif i == "x":
+                    self.x = j
+                elif i == "y":
+                    self.y = j
+
+    def __str__(self):
+        """Return the print() and str() representation of a Square"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
